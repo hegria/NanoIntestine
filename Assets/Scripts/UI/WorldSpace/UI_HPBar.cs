3b39aa9f866797f1d7de52e8206995331198d455
hegria <awzs159@gmail.com>
2022-08-19 21:09:17 +0900

    helloworld

---
+++ Assets/Scripts/UI/WorldSpace/UI_HPBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HPBar : UI_Base
+{
+    enum GameObjects
+    {
+        HPBar
+    }
+
+
+    public override void Init()
+    {
+        Bind<GameObject>(typeof(GameObjects));
+    }
+
+    private void Update()
+    {
+        Transform parent = transform.parent;
+        transform.position = parent.position + Vector3.up * (parent.GetComponent<Collider>().bounds.size.y);
+        transform.rotation = Camera.main.transform.rotation;
+        float ratio = 0f;
+        SetHpRatio(ratio);
+    }
+
+    public void SetHpRatio(float ratio)
+    {
+        GetObject((int)GameObjects.HPBar).GetComponent<Slider>().value = ratio;
+    }
+}
